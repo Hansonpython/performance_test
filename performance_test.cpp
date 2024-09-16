c3f2b7551d8d33a534751ad7360267376514fd65
Hansonpython <149308676+Hansonpython@users.noreply.github.com>
2024-09-16 19:51:42 +0800

    (English Language) Update and rename 测试性能.c to performance_test.cpp

--- performance_test.cpp
+++ performance_test.cpp
@@ -15,9 +15,9 @@ int main()
     system("clear");
 #endif
     float a = 3.1415926535898, b = 30.1415926535898, c = 300.14159265358, d;
-    cout << "将要进行算力测试:" << endl << "    执行十亿 ( " << T << " ) 次 d = a * b + c 计算。" << endl << "    a = " << a << " b = " << b << " c = " << c << " d 未初始化。" << endl;
+    cout << "Will conduct a computing power test:" << endl << "Execute one billion ( "<< T << " ) calculations d = a * b + c << endl << "    a = " << a << " b= " << b << " c= " << c << " d is not initialized. " << endl;
     for (int i = 0 ; i < T ; i++) d = a * b + c;
     double result = (double)T / ((double)clock() / CLOCKS_PER_SEC);
-    cout << setprecision(0) << "您的计算机一秒能执行 (向下取整) " << result << " 次运算。(相当于 " << result / T * 10 << " 亿次运算)。" << endl;
+    cout << setprecision(0) << "Your computer can perform one second (rounded down) " << result << " operations. (Equivalent to " << result / T * 10 << " on hundred million operations)." << endl;
     return 0;
 }
